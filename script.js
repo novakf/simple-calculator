7d0190fc1d1dc5fadf9e518b30db96fc7cbd7dc1
novakf <bogdannovikov116@mail.ru>
2024-01-29 15:01:20 +0300

    upload

---
+++ script.js
@@ -0,0 +1,100 @@
+window.onload = function(){ 
+    let a = ''
+    let b = ''
+    let expressionResult = ''
+    let selectedOperation = null
+
+    outputElement = document.getElementById("result")
+
+    digitButtons = document.querySelectorAll('[id ^= "btn_digit_"]')
+
+    function onDigitButtonClicked(digit) {
+        if (!selectedOperation) {
+            if ((digit != '.') || (digit == '.' && !a.includes(digit))) { 
+                a += digit
+            }
+            outputElement.innerHTML = a
+        } else {
+            if ((digit != '.') || (digit == '.' && !b.includes(digit))) { 
+                b += digit
+                outputElement.innerHTML = b        
+            }
+        }
+    }
+
+    // устанавка колбек-функций на кнопки циферблата по событию нажатия
+    digitButtons.forEach(button => {
+        button.onclick = function() {
+            const digitValue = button.innerHTML
+            onDigitButtonClicked(digitValue)
+        }
+    });
+
+    // установка колбек-функций для кнопок операций
+    document.getElementById("btn_op_mult").onclick = function() { 
+        if (a === '') return
+        selectedOperation = 'x'
+    }
+    document.getElementById("btn_op_plus").onclick = function() { 
+        if (a === '') return
+        selectedOperation = '+'
+    }
+    document.getElementById("btn_op_minus").onclick = function() { 
+        if (a === '') return
+        selectedOperation = '-'
+    }
+    document.getElementById("btn_op_div").onclick = function() { 
+        if (a === '') return
+        selectedOperation = '/'
+    }
+
+    document.getElementById("btn_op_back").onclick = function() {
+        if (a.length > 1) {
+            a = a.slice(0,-1) 
+            outputElement.innerHTML = a
+        }
+        else {
+            a = ''
+            b = ''
+            selectedOperation = ''
+            expressionResult = ''
+            outputElement.innerHTML = 0
+        }
+        
+    }
+
+    // кнопка очищения
+    document.getElementById("btn_op_clear").onclick = function() { 
+        a = ''
+        b = ''
+        selectedOperation = ''
+        expressionResult = ''
+        outputElement.innerHTML = 0
+    }
+
+    // кнопка расчёта результата
+    document.getElementById("btn_op_equal").onclick = function() { 
+        if (a === '' || b === '' || !selectedOperation)
+            return
+
+        switch(selectedOperation) { 
+            case 'x':
+                expressionResult = (+a) * (+b)
+                break;
+            case '+':
+                expressionResult = (+a) + (+b)
+                break;
+            case '-':
+                expressionResult = (+a) - (+b)
+                break;
+            case '/':
+                expressionResult = (+a) / (+b)
+                break;
+        }
+
+        a = expressionResult.toString()
+        b = ''
+        selectedOperation = null
+        outputElement.innerHTML = a
+    }
+};
